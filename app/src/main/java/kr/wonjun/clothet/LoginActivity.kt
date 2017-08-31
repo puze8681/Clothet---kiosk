d3b2abe8c7cfb033e5978dd4d571f3823d5cfe70
puze8681 <rsef0601@naver.com>
2017-09-01 07:23:03 +0900

    setting

--- app/src/main/java/kr/wonjun/clothet/LoginActivity.kt
+++ app/src/main/java/kr/wonjun/clothet/LoginActivity.kt
@@ -2,11 +2,22 @@ package kr.wonjun.clothet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        splash_btn_login.setOnClickListener {
+            startActivity<MainActivity>()
+        }
+
+        splash_btn_register.setOnClickListener {
+            startActivity<RegisterActivity>()
+        }
+
+
     }
 }
